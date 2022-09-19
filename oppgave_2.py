f6a46aabff6e17b4af2c142317f2188031837d30
malineri <malineri@uio.no>
2022-09-19 16:19:55 +0200

    made python program for poisson task 2

---
+++ oppgave_2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def poisson(x):
+    u = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
+    return u
+
+n = 5
+x = np.linspace(0, 1, n + 1)
+P = poisson(x)
+
+
+res = "\n".join("{} {}".format(x, y) for x, y in zip(x, P))
+print(res)
+
+
+n = 100
+x = np.linspace(0, 1, n + 1)
+P = poisson(x)
+
+plt.title("Plot of one-dimetional Poisson equation")
+plt.plot(x, P)
+plt.xlabel("x from 0 to 1")
+plt.ylabel("The corresponding values u(x)")
+plt.show()
