1f34423770d03c99a8793ec9ef14d909b6fb7f91
malineri <malineri@uio.no>
2022-09-20 12:09:37 +0200

    changed the script by removing the print of values and doing this in another program with c++

--- plot.py
+++ plot.py
@@ -6,14 +6,6 @@ def poisson(x):
     u = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
     return u
 
-n = 5
-x = np.linspace(0, 1, n + 1)
-P = poisson(x)
-
-
-res = "\n".join("{} {}".format(x, y) for x, y in zip(x, P))
-print(res)
-
 
 n = 100
 x = np.linspace(0, 1, n + 1)
