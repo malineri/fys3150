8e2a45b3f7846b737c3f75a2cafa4c1f7341ead7
malineri <malineri@uio.no>
2022-09-20 14:25:14 +0200

    made a simple program for the equation Av = g, using Armadillo

---
+++ matrices.cpp
@@ -0,0 +1,19 @@
+#include <armadillo>
+
+
+int main()
+{
+  int n = 4
+  int m = 4
+  arma::vec v = arma::vec(n);
+  arma::vec g = arma::vec(m);
+
+  //creating the matrix A
+  arma::mat A = arma::mat(n, m);
+
+  //adding elements to the matrix A
+  }
+
+
+return 0;
+}
