350ace7dc36bc9c955dab038e02f7d7caf54456f
malineri <malineri@uio.no>
2022-09-20 16:35:30 +0200

    made changes to the program trying to add to the matrix

--- matrices.cpp
+++ matrices.cpp
@@ -1,4 +1,8 @@
+
+#include <iostream>
+#include <vector>
 #include <armadillo>
+
 
 
 int main()
@@ -12,7 +16,16 @@ int main()
   arma::mat A = arma::mat(n, m);
 
   //adding elements to the matrix A
+  A(1,1)= 2; //Assign element (i,j) of the matrix A to A_ij.
+
+  std::cout << "#" << std::setw(width-1) << "A"
+            << std::endl;
+
+  for (double i; i <x.size(); i++){
+    std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << A
+              << std::endl;
   }
+
 
 
 return 0;
