7ddada7a7e90ad7dc4509df7e950d750b2c12b51
malineri <malineri@uio.no>
2022-10-08 19:17:00 +0200

    finally got armadillo working, made a simple program with zero matrix

--- project_2/problem_2.cpp
+++ project_2/problem_2.cpp
@@ -1,22 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
 #include <armadillo>
 
-
+//Creating a program that takes an NxN Matrix A filled with random numbers,
+//and generate a symmentric matrix B, where the eigenval and eigenvec are the respective
+//eigenvalues and eigenvectors of the matrix B.
 
 int main()
 {
-  int N = 6
-  mat A(N, N, fill::randu);
-  vec v = std::vector<int> v2(N);
+  int N = 6;
 
-  vec eigval;
-  mat eigvec;
+  arma::mat A(N, N).fill(0.); //create NxN matrix with zeros
+  arma::mat B = A.t()*A; //create symmetric matrix
 
-  eig_sym(eigval, eigvec, B);
+  arma::diagmat(arma::sum(A, 1)).print("B");
 
-  }
+
+  arma::vec eigval;
+  arma::mat eigvec;
+
+  arma::eig_sym(eigval, eigvec, B);
+
+  std::cout<<B
+           <<std::endl;
+
+
+
+
+
+
+  // Parameters for output formatting
+  int width = 18;
+  int prec  = 10;
+
+
+  //output Eigenvectors and Eigenvalues
+  std::cout << "#" << std::setw(width-1) << "Eigenvectors"
+            << std::endl;
+
+
+  std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << eigvec
+            << std::endl;
+
+
+
+  std::cout << std::setw(width - 1) << "Eigenvalues"
+            << std::endl;
+
+  std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << eigval
+            << std::endl;
+
+
+
+
+
+
 
   return 0;
 
-}  
+}
